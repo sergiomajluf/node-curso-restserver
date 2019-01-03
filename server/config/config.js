597d3f9575e73fda4ba0aa06e1b8168351d5fe5f
Sergio <sergio.majluf@gmail.com>
2019-01-03 14:10:27 -0300

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,24 @@
+// ============================
+//  Puerto
+// ============================
+process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    // urlDB = 'mongodb://cafe-usuario:A123456@ds113785.mlab.com:13785/cafe'
+    urlDB = process.env.MONGO_URI;
+}
+process.env.URLDB = urlDB;
